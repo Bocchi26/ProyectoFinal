ef5129c78b0530e7c407e22e48f801afb7e35cec
Albert Chacon <roronoaalbert2@gmail.com>
2024-11-23 16:46:05 -0500

    cambios de color de la plantilla

--- src/components/Plantilla/plantilla.js
+++ src/components/Plantilla/plantilla.js
@@ -1,14 +1,16 @@
 
 import React from "react";
 import BarraNavegacion from "./barraNavegacion";
+import "./plantilla.css"
 
 const Plantilla = ({ children }) => {
   return (
-    <div style={{ display: "flex", minHeight: "100vh" }}>
+    <div className="planti">
         {/* Barra de navegación */}
         <BarraNavegacion />
          {/* aqui va a ir como va a ser la plntilla */}
       <div className="contenidoPrincipal">
+
         {children} {/* Contenido específico de cada página */}
       </div>
     </div>
