53c837d0508b13063343f0c1eb21aeba54387a93
Albert Chacon <roronoaalbert2@gmail.com>
2024-11-29 15:36:48 -0500

    arreglando otra vez esta maricada

--- src/components/DatosHistoricos/historia.js
+++ src/components/DatosHistoricos/historia.js
@@ -1,21 +1,55 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Plantilla from "../Plantilla/plantilla";
 
+const DatosHistoricos = () => {
+  useEffect(() => {
+    const cargarCSV = async () => {
+      try {
+        const respuestas = await fetch("/archivosCSV/02 modern-renewable-energy-consumption.csv");
+        const texto = await respuestas.text();
 
-const datosHistoricos = () => {
-  
+        const lineas = texto.split("\n").map((linea) => linea.trim());
+        const encabezados = lineas[0].split(",");
+        const cuerpo = lineas.slice(1);
+
+        const tbody = document.querySelector("#tablaUsuarios tbody");
+
+        cuerpo.forEach((linea) => {
+          if (linea.trim() === "") return;
+          const columnas = linea.split(",");
+          if (columnas.length === encabezados.length) {
+            const fila = document.createElement("tr");
+
+            columnas.forEach((dato) => {
+              const celda = document.createElement("td");
+              celda.textContent = dato.trim();
+              fila.appendChild(celda);
+            });
+
+            tbody.appendChild(fila);
+          }
+        });
+      } catch (error) {
+        console.error("Error al cargar el archivo CSV: ", error);
+      }
+    };
+
+
+    cargarCSV();
+  }, []); // Se ejecuta al montar el componente
+
   return (
     <Plantilla>
       <h1>Bienvenido a la Página de la Calculadora</h1>
       <p>Este es el contenido único de la página Calculadora.</p>
       <div>
-        <h1>Tabla de pruebitaa</h1>
+        <h1>Tabla de prueba</h1>
         <table id="tablaUsuarios">
           <thead>
             <tr>
               <th>PAIS</th>
               <th>CODIGO</th>
-              <th> AÑO</th>
+              <th>AÑO</th>
               <th>GEOTERMICA (TWH)</th>
               <th>ENERGIA SOLAR (TWH)</th>
               <th>ENERGIA EOLICA (TWH)</th>
@@ -29,6 +63,5 @@ const datosHistoricos = () => {
   );
 };
 
-
-export default datosHistoricos;
+export default DatosHistoricos;
 
