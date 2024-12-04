091c6789beb9e02a7354f45086278237b7342f74
Diego Patiño <patinodiego1005@gmail.com>
2024-12-03 19:07:26 -0500

    Proyecto semi-terminado

--- src/components/Plantilla/plantilla.js
+++ src/components/Plantilla/plantilla.js
@@ -11,11 +11,7 @@ const Plantilla = ({ children }) => {
         <img src= "https://i.ibb.co/6F1z6ms/Captura-de-pantalla-2024-11-23-184323-removebg-preview.png" alt=" " id="logo"/> 
         <h2 id="texto-encabezado">GeoTech</h2>
 
-        <div className="links">
-            <a href="#SubTitulo">¿Qué es?</a>
-            <a href="#subTitulo2">Beneficios</a>
-            <a href="#titulosecciones">Aplicación</a>
-        </div>
+        
 
       </header>
 
